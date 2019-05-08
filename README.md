6a59398d9d2deb77c2cad9886ea7df46c4fd8ca2
Zaki Malik <malik.zaki@hotmail.com>
2019-05-08 23:46:53 +0500

    Merge dev in master (#3)
    * bugfix to use context logger and send json response for default case
    
    * added env variable info to readme
    
    * formatting fix for table in readme.md
    
    * indexed model for search optimization
    
    * added lodash to be used as a JS helper module
    
    * changes for search for assigned and unassigned users
    
    * changes for improving result relevance, comments added as well
    
    * added assign user functionality by id and in bulk
    
    * added exception handling and revised response message
    
    * added postman collection file to project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # azure-func-demo
 A repository for creating sample azure functions (serverless) using Typescript and VS Code
+
+**Environment variables**
+
+| Name        | Description                                                           |
+|-------------|-----------------------------------------------------------------------|
+| MONGODB_URI | The Mongoose connection string needed to connect to a MongoDB Cluster |
--- Users/index.ts
+++ Users/index.ts
@@ -1,5 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import * as mongoose from 'mongoose';
+import * as _ from 'lodash';
+const User = require('../models/User');
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
@@ -10,7 +12,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
     mongoose.connection.on('error', (err) => {
-        console.log(`ERROR→ ${err.message}`);
+        context.log(`ERROR→ ${err.message}`);
     });
 
     switch(req.method){
@@ -22,16 +24,22 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         case "PATCH":
             await updateUserById(context, req);
             break;
+        case "PUT":
+            await updateUsersInBulk(context, req);
+            break;
         // This will return a 404 for all methods other than get and patch that have been allowed in function.json file under bindings[0].methods
         default:
-            context.res = {
-                status: 404,
-                body: `The route does not exist`
-            };
+            sendErrorResponse(404, new Error(`The route method is not applicable for this resource`), context);
     }
 };
 
 /**
+ * This function supports:
+ * 1. filtering users collection, by default it filters unassigned users. To filter assigned users the query param assigned needs to be set to true or any numeric value > 0.
+ * 2. searching users collection. The "name" query parameter needs to be passed with the value as the search term
+ * 3. results are paginated. By default pageNumber is 1, pageSize is 15. These defaults can be overridden by passing in query params page and size respectively
+ * 
+ * Note: Results contain a meta object detailing the page, size and total results
  * 
  * @param context 
  * @param req 
@@ -39,17 +47,60 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 const getAllUsers = async function (context: Context, req: HttpRequest): Promise<void> {
     try{
         context.log(`inside getAllUsers fn`);
-        const User = require('./../models/User.js');
-        const {size=null, page=null} = req.query;
+        const {size=null, page=null, name=null, assigned=false} = req.query;
         let pageSize = (size && parseInt(size)) ? parseInt(size) : 15, 
             pageNum = (page && parseInt(page)) ? parseInt(page) : 1,
             skipVal = pageSize * (pageNum - 1);
-        context.log(`pageSize: ${pageSize}, pageNum: ${pageNum}, skipVal: ${skipVal}`);
+        let isAssigned = (assigned && ( Number(assigned) > 0 || assigned.toLowerCase().trim() === "true")) ? true : false;    
+        context.log(`pageSize: ${pageSize}, pageNum: ${pageNum}, skipVal: ${skipVal}, isAssigned: ${isAssigned}`);
 
+        // By default query object will filter all users that have not been assigned yet
+        let queryObj = {
+            assigned: false
+        };
+
+        // By default there is no sorting -- will only be used for searches and to rank text scores
+        let sortObj = {};
         
-        const allowedFilterFields = [];
-        let query = {};
-        const promiseArr = [User.count(query), User.find(query).skip(skipVal).limit(pageSize)];
+        /**
+         * name will be defined in case of search -- add search criteria to queryObj, sortObj (to sort on text score)
+         * solution for acheiving partial auto-complete functionality with relevance
+         * 1. do text search -- text index is already defined on name.first and name.last this will help in improving relevance as searchTerm size increases
+         * 2. do a regex search -- regexes based on start anchors will help provide results for partial filtering 
+         * */
+        if(name){
+            let nameRegex = new RegExp(`^${name}`, "i");    
+            context.log(`nameRegex: ${nameRegex}`);
+
+            queryObj["$or"] = [
+                {
+                    $text: {
+                        $search: name
+                    } 
+                }, 
+                {
+                    "name.first":{
+                        $regex: nameRegex,
+                        $options: "i"
+                    }
+                },
+                {
+                    "name.last":{
+                        $regex: nameRegex,
+                        $options: "i"
+                    }
+                }
+            ];
+            
+            sortObj = { score: { $meta: "textScore" } };
+        }
+
+        // this flag will be set when filtering / searching for assigned users -- add search criteria to queryObj
+        if(isAssigned){
+            queryObj.assigned = true;
+        }
+        context.log(`queryObj: ${JSON.stringify(queryObj)}`);
+        const promiseArr = [User.countDocuments(queryObj), User.find(queryObj, sortObj).sort(sortObj).skip(skipVal).limit(pageSize)];
         const resultArr =  await Promise.all(promiseArr);
         const [total, results] = resultArr;
         const meta = {
@@ -73,18 +124,106 @@ const getAllUsers = async function (context: Context, req: HttpRequest): Promise
  * @param req 
  */
 const updateUserById = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log(`inside updateUserById fn`);
-    const userId = (req.params && req.params.id) ? req.params.id :  null;
-    
-    if (userId) {
-        sendResponse(200, {
-            message : `received userId: ${userId}`
-        }, context);
+    try{
+        context.log(`inside updateUserById fn`);
+        const userId = (req.params && req.params.id) ? req.params.id :  null;
+        const allowedFields = ["assigned"];
+        if (userId && req.body && mongoose.Types.ObjectId.isValid(userId)) {
+            let objToSet = {};
+            for( let key in req.body){
+                if(allowedFields.includes(key)){
+                    objToSet[key] = req.body[key];
+                }
+            }
+            if(Object.keys(objToSet).length){
+                let updatedUser = await User.findByIdAndUpdate(userId, {$set: objToSet}, {new: true});
+                if(updatedUser) {
+                  sendResponse(200, {
+                    message: `user has been updated successfully`,
+                    user: updatedUser
+                  }, context);
+                } else {
+                  sendResponse(404,{
+                      message: `The specified user does not exist`
+                  }, context);
+                }        
+            }else{
+                context.log(`body doesn't contain modifiable fields, nothing to update`);
+                sendResponse(400, {
+                    message : `Mandatory parameters missing or in invalid format`
+                }, context);
+            }
+        }
+        else {
+            context.log(`req.params is missing userId`);
+            sendResponse(400, {
+                message : `Mandatory parameters missing in invalid format`
+            }, context);
+        }
+    }catch(e){ 
+        sendErrorResponse(500, e, context);
     }
-    else {
-        sendResponse(400, {
-            message : `Please pass a userId, it is a mandatory parameter`
-        }, context);
+};
+
+/**
+ * 
+ * @param context 
+ * @param req 
+ */
+const updateUsersInBulk = async function (context: Context, req: HttpRequest): Promise<void> {
+    try{
+        context.log(`inside updateUsersInBulk fn`);
+        const userIds = (req.body && req.body.userIds) ? req.body.userIds :  [];
+        const validFilteredIds = userIds.filter((userId) => mongoose.Types.ObjectId.isValid(userId));
+        let meta = {
+            success: false,
+            receivedIdsLength: userIds.length,
+            validFilteredIdsLength: validFilteredIds.length,
+            usersUpdatedLength: 0,
+            usersMatchedLength: 0,
+        };
+
+        //Only allowed field to update currently is the assigned key
+        // NOTE: Do not add the _id and userIds field in allowedFields array
+        const allowedFields = ["assigned"];
+
+        if (validFilteredIds.length) {
+            let objToSet = {};
+                for( let key in req.body){
+                    if(allowedFields.includes(key)){
+                        objToSet[key] = req.body[key];
+                    }
+                }
+                if(Object.keys(objToSet).length){
+                    let updatedUsersMeta = await User.update({_id: validFilteredIds}, {$set: objToSet}, {new: true, multi: true});
+                    console.log(`updatedUsersMeta: ${JSON.stringify(updatedUsersMeta)}`);
+                    if(updatedUsersMeta) {
+                        const {ok:success=false, nModified:docsModifiedCount=0, n:docsMatched=0} = updatedUsersMeta;
+                        meta.success = (success) ? true : false;
+                        meta.usersMatchedLength = docsMatched;
+                        meta.usersUpdatedLength = docsModifiedCount;
+                        sendResponse(200, {
+                            details: meta,
+                            message: `Operation ${meta.success ? "successful" : "unsuccessful"},  ${meta.usersMatchedLength} User document(s) matched, ${meta.usersUpdatedLength} document(s) updated`
+                        }, context);
+                    } else {
+                    sendResponse(404,{
+                        message: `The specified user(s) do not exist and were not updated`
+                    }, context);
+                    }        
+                }else{
+                    context.log(`body doesn't contain modifiable fields, nothing to update`);
+                    sendResponse(400, {
+                        message : `Mandatory parameters missing or in invalid format`
+                    }, context);
+                }
+        }else {
+            sendResponse(400, {
+                message : `Please pass in valid userIds`
+            }, context);
+        }
+    }catch(e){ 
+        sendErrorResponse(500, e, context);
     }
 };
 
@@ -102,7 +241,7 @@ const sendResponse = async function (status: number, body: object, context: Cont
             'Content-Type': 'application/json'
         }
     };
-    context.log(`returning response from route invocation: ${JSON.stringify(response)}`);
+    // context.log(`returning response from route invocation: ${JSON.stringify(response)}`);
     context.res = response;
 };
 
