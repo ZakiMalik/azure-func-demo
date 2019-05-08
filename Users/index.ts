357a687880b3fbbe292a91e421081e2ea2d4b0e0
Zaki Malik <malik.zaki@hotmail.com>
2019-05-08 15:17:23 +0500

    changes for improving result relevance, comments added as well

--- Users/index.ts
+++ Users/index.ts
@@ -1,6 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import * as mongoose from 'mongoose';
 import * as _ from 'lodash';
+const User = require('../models/User');
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
@@ -30,6 +31,12 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 };
 
 /**
+ * This function supports:
+ * 1. filtering users collection, by default it filters unassigned users. To filter assigned users the query param assigned needs to be set to true or any numeric value > 0.
+ * 2. searching users collection. The "name" query parameter needs to be passed with the value as the search term
+ * 3. results are paginated. By default pageNumber is 1, pageSize is 15. These defaults can be overridden by passing in query params page and size respectively
+ * 
+ * Note: Results contain a meta object detailing the page, size and total results
  * 
  * @param context 
  * @param req 
@@ -37,7 +44,6 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 const getAllUsers = async function (context: Context, req: HttpRequest): Promise<void> {
     try{
         context.log(`inside getAllUsers fn`);
-        const User = require('./../models/User.js');
         const {size=null, page=null, name=null, assigned=false} = req.query;
         let pageSize = (size && parseInt(size)) ? parseInt(size) : 15, 
             pageNum = (page && parseInt(page)) ? parseInt(page) : 1,
@@ -45,40 +51,53 @@ const getAllUsers = async function (context: Context, req: HttpRequest): Promise
         let isAssigned = (assigned && ( Number(assigned) > 0 || assigned.toLowerCase().trim() === "true")) ? true : false;    
         context.log(`pageSize: ${pageSize}, pageNum: ${pageNum}, skipVal: ${skipVal}, isAssigned: ${isAssigned}`);
 
-        let queryObj = {};
+        // By default query object will filter all users that have not been assigned yet
+        let queryObj = {
+            assigned: false
+        };
+
+        // By default there is no sorting -- will only be used for searches and to rank text scores
+        let sortObj = {};
+        
+        /**
+         * name will be defined in case of search -- add search criteria to queryObj, sortObj (to sort on text score)
+         * solution for acheiving partial auto-complete functionality with relevance
+         * 1. do text search -- text index is already defined on name.first and name.last this will help in improving relevance as searchTerm size increases
+         * 2. do a regex search -- regexes based on start anchors will help provide results for partial filtering 
+         * */
         if(name){
-            let nameRegex = new RegExp(`^${name}`, "i");
-                
+            let nameRegex = new RegExp(`^${name}`, "i");    
             context.log(`nameRegex: ${nameRegex}`);
-            queryObj = { 
-                $or:[
-                    {
-                        $text: {
-                            $search: name
-                        } 
-                    }, 
-                    {
-                        "name.first":{
-                            $regex: nameRegex,
-                            $options: "i"
-                        }
-                    },
-                    {
-                        "name.last":{
-                            $regex: nameRegex,
-                            $options: "i"
-                        }
+
+            queryObj["$or"] = [
+                {
+                    $text: {
+                        $search: name
+                    } 
+                }, 
+                {
+                    "name.first":{
+                        $regex: nameRegex,
+                        $options: "i"
                     }
-                ]
-                
-            };
+                },
+                {
+                    "name.last":{
+                        $regex: nameRegex,
+                        $options: "i"
+                    }
+                }
+            ];
+            
+            sortObj = { score: { $meta: "textScore" } };
         }
 
+        // this flag will be set when filtering / searching for assigned users -- add search criteria to queryObj
         if(isAssigned){
-            queryObj["assigned"] = true;
+            queryObj.assigned = true;
         }
         context.log(`queryObj: ${JSON.stringify(queryObj)}`);
-        const promiseArr = [User.countDocuments(queryObj), User.find(queryObj).skip(skipVal).limit(pageSize)];
+        const promiseArr = [User.countDocuments(queryObj), User.find(queryObj, sortObj).sort(sortObj).skip(skipVal).limit(pageSize)];
         const resultArr =  await Promise.all(promiseArr);
         const [total, results] = resultArr;
         const meta = {
