44d889fbabb754cec8d7ccd0c19667d1a3ea1d19
Zaki Malik <malik.zaki@hotmail.com>
2019-05-11 13:57:47 +0500

    Added remaining functional tests

--- Users/index.test.ts
+++ Users/index.test.ts
@@ -1,7 +1,8 @@
 import httpFunction from './index';
 import context = require('azure-function-context-mock');
 import { HttpRequest } from '@azure/functions';
-process.env.MONGODB_URI = require('../local.settings.json').Values.MONGODB_URI_TEST;
+const User = require('../models/User');
+process.env.MONGODB_URI = require('./../local.settings.json').Values.MONGODB_URI;
 
 describe('User List => GET /users[?page=&size=&name=&assigned=]', () => {
     
@@ -360,13 +361,14 @@ describe('User List => GET /users[?page=&size=&name=&assigned=]', () => {
 
 describe('Edit a User => PATCH /users/:id', () => {
     
-    test('should return an error if required url parameter userId is not present', async () => {
+    test('should return an error 400 if required url parameter userId is not present', async () => {
         const request: HttpRequest = {
             method: "PATCH" ,
             url: "",
             headers:{},
             query:{},
-            params:{}  
+            params:{},
+            body:{}  
         };
     
         await httpFunction(context, request);
@@ -376,19 +378,185 @@ describe('Edit a User => PATCH /users/:id', () => {
         expect(res.status).toEqual(400);
         expect(res.body).toBeDefined();
         expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toContain(`Mandatory parameters missing`);
+    });
+
+    test('should return a 400 Bad Request if required url parameter userId is not ObjectId, provided the body contains editable field(s)', async () => {
+        const request: HttpRequest = {
+            method: "PATCH" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{
+                id: "123"
+            },
+            body:{
+                assigned : false
+            }  
+        };
+    
+        await httpFunction(context, request);
+    
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).toEqual(400);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toContain('Mandatory parameters missing or in invalid format');
+    });
+
+    test('should return a 400 Bad Request if provided userId is valid but body does not contain editable field(s)', async () => {
+        const unsavedUserId = new User()._id;
+
+        const request: HttpRequest = {
+            method: "PATCH" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{
+                id: unsavedUserId
+            },
+            body:{
+                someOtherField : false
+            }  
+        };
+    
+        await httpFunction(context, request);
+    
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).toEqual(400);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toContain('No Editable Fields');
+    });
+
+    test('should return a 404 Not Found if provided userId is valid but user does not exist, provided the body contains editable field(s)', async () => {
+        const unsavedUserId = new User()._id;
+
+        const request: HttpRequest = {
+            method: "PATCH" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{
+                id: unsavedUserId._id
+            },
+            body:{
+                assigned : false
+            }  
+        };
+    
+        await httpFunction(context, request);
+    
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).toEqual(404);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+    });
+
+    test('should return a 200 OK Found if provided userId is valid, user does exist, provided the body contains editable field(s)', async () => {
+        const unsavedUser = new User({
+            name :{
+                title: "mr",
+                first: "Zaki",
+                last: "Malik" 
+            },
+            picture: {
+                large: "/path/to/large/pic",
+                medium: "/path/to/medium/pic",
+                thumbnail: "/path/to/thumbnail/pic"
+            },
+            assigned: false
+        });
+        let savedUser = await unsavedUser.save();
+        let savedUserId = savedUser._id;
+
+
+
+        const request: HttpRequest = {
+            method: "PATCH" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{
+                id: savedUserId
+            },
+            body:{
+                assigned : true
+            }  
+        };
+    
+        await httpFunction(context, request);
+    
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).toEqual(200);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+        expect(res.body).toHaveProperty('user');
+        expect(res.body.message).toContain('updated successfully');
+        expect(res.body.user).toHaveProperty('assigned');
+        expect(res.body.user.assigned).toBeTruthy();
+
+        await unsavedUser.remove();
     });
     
+    test('should return a non 200 Http Status if provided userId is valid, user does exist, provided the body contains editable field(s) but in invalid format', async () => {
+        const unsavedUser = new User({
+            name :{
+                title: "mr",
+                first: "Zaki",
+                last: "Malik" 
+            },
+            picture: {
+                large: "/path/to/large/pic",
+                medium: "/path/to/medium/pic",
+                thumbnail: "/path/to/thumbnail/pic"
+            },
+            assigned: false
+        });
+        let savedUser = await unsavedUser.save();
+        let savedUserId = savedUser._id;
+
+
+
+        const request: HttpRequest = {
+            method: "PATCH" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{
+                id: savedUserId
+            },
+            body:{
+                assigned : 123
+            }  
+        };
+    
+        await httpFunction(context, request);
+    
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).not.toEqual(200);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+
+        await unsavedUser.remove();
+    });
 });
 
 describe('Bulk Edit multiple Users => PUT /users', () => {
     
-    test('should return an error if required body parameters userIds, assigned are not present', async () => {
+    test('should return a 400 Bad Request if required body parameters userIds are not present', async () => {
         const request: HttpRequest = {
             method: "PUT" ,
             url: "",
             headers:{},
             query:{},
-            params:{}  
+            params:{},
+            body:{}  
         };
 
         await httpFunction(context, request);
@@ -398,8 +566,201 @@ describe('Bulk Edit multiple Users => PUT /users', () => {
         expect(res.status).toEqual(400);
         expect(res.body).toBeDefined();
         expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toContain('Please pass in valid userIds');
     });
 
+    test('should return a non 200 Http Status if required url parameter userIds is not an array, provided body contains editable field(s)', async () => {
+        const request: HttpRequest = {
+            method: "PUT" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{},
+            body:{
+                userIds: "invalid string not an array",
+                assigned: true
+            }  
+        };
+
+        await httpFunction(context, request);
+
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).toEqual(400);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toContain('Please pass in valid userIds');
+    });
+
+    test('should return a 400 Bad Request if array of userIds provided are not valid ObjectIds', async () => {
+        const request: HttpRequest = {
+            method: "PUT" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{},
+            body:{
+                userIds: ["abc123", "def456"],
+                assigned: true
+            }  
+        };
+
+        await httpFunction(context, request);
+
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).toEqual(400);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toContain('Please pass in valid userIds');
+    });
+
+    test('should return a 400 Bad Request if provided userIds are valid but body does not contain editable field(s)', async () => {
+        const unsavedUserIds = [new User()._id, new User()._id];
+
+        const request: HttpRequest = {
+            method: "PUT" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{},
+            body:{
+                userIds: unsavedUserIds,
+                someOtherField : true
+            }  
+        };
+    
+        await httpFunction(context, request);
+    
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).toEqual(400);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toContain('No Editable Fields');
+    });
+
+    test('should return a 404 Not Found if provided userIds are valid but no user matches, provided the body contains editable field(s)', async () => {
+        const unsavedUserIds = [new User()._id, new User()._id];
+
+        const request: HttpRequest = {
+            method: "PUT" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{},
+            body:{
+                userIds: unsavedUserIds,
+                assigned : true
+            }  
+        };
+    
+        await httpFunction(context, request);
+    
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).toEqual(404);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+        expect(res.body.message).toContain('No document(s) matched');
+    });
+ 
+    test('should return a 200 OK Found if provided userId(s) are valid, user(s) do exist, provided the body contains editable field(s)', async () => {
+        const unsavedUser = new User({
+            name :{
+                title: "mr",
+                first: "Zaki",
+                last: "Malik" 
+            },
+            picture: {
+                large: "/path/to/large/pic",
+                medium: "/path/to/medium/pic",
+                thumbnail: "/path/to/thumbnail/pic"
+            },
+            assigned: false
+        });
+        let savedUser = await unsavedUser.save();
+        let savedUserId = savedUser._id;
+
+
+
+        const request: HttpRequest = {
+            method: "PUT" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{},
+            body:{
+                userIds: [savedUserId],
+                assigned : true
+            }  
+        };
+    
+        await httpFunction(context, request);
+    
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).toEqual(200);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+
+        expect(res.body).toHaveProperty('details');
+        expect(res.body.details).toHaveProperty('receivedIdsLength');
+        expect(res.body.details).toHaveProperty('success');
+        expect(res.body.details).toHaveProperty('usersMatchedLength');
+        expect(res.body.details).toHaveProperty('usersUpdatedLength');
+        expect(res.body.details).toHaveProperty('validFilteredIdsLength');
+
+        expect(res.body.details.success).toBeTruthy();
+        expect(res.body.details.usersMatchedLength).toBeGreaterThanOrEqual(1);
+
+
+
+        await unsavedUser.remove();
+    });
+    
+    test('should return a non 200 Http Status if provided userId(s) are valid, user(s) do not exist, provided the body contains editable field(s) but in invalid format', async () => {
+        const unsavedUser = new User({
+            name :{
+                title: "mr",
+                first: "Zaki",
+                last: "Malik" 
+            },
+            picture: {
+                large: "/path/to/large/pic",
+                medium: "/path/to/medium/pic",
+                thumbnail: "/path/to/thumbnail/pic"
+            },
+            assigned: false
+        });
+        let savedUser = await unsavedUser.save();
+        let savedUserId = savedUser._id;
+
+
+
+        const request: HttpRequest = {
+            method: "PUT" ,
+            url: "",
+            headers:{},
+            query:{},
+            params:{},
+            body:{
+                userIds : [savedUserId],
+                assigned : 123
+            }  
+        };
+    
+        await httpFunction(context, request);
+    
+        const res = context.res;
+        expect(res).toBeDefined();
+        expect(res.status).not.toEqual(200);
+        expect(res.body).toBeDefined();
+        expect(res.body).toHaveProperty('message');
+
+        await unsavedUser.remove();
+    });
+ 
 });
 
 describe('Not Applicable Http Methods => POST /users', () => {
